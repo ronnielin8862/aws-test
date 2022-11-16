3aee49b162734b8b9be3829d0883c5510f82c4bd
Ronnie <ronnielin8862@gmail.com>
2022-11-16 22:31:48 +0800

    嘗試不同的completableFuture玩法

--- src/test/java/multiThread/CompleteFuture.java
+++ src/test/java/multiThread/CompleteFuture.java
@@ -17,7 +17,38 @@ public class CompleteFuture {
 
         System.out.println(a.join() + b.join() + c.join());
 
+
         System.out.println("total time = " + (System.currentTimeMillis() - st));
+    }
+
+    @Test
+    public void oneThreadReturn(){
+        long st = System.currentTimeMillis();
+
+        CompletableFuture<String> a = CompletableFuture.supplyAsync(()-> {
+            var a1 = he();
+            var a2 = spa();
+            var a3 = wo();
+            return a1+a2+a3;
+        });
+
+        System.out.println(a.join());
+
+        System.out.println("total time = " + (System.currentTimeMillis() - st));
+    }
+
+    @Test
+    public void oneThreadRun() throws InterruptedException {
+        CompletableFuture.runAsync(()-> {
+            var a1 = he();
+            var a2 = spa();
+            var a3 = wo();
+
+            System.out.println(a1+a2+a3);
+        });
+
+        Thread.sleep(4500);
+        System.out.println("finish");
     }
 
     public String he(){
